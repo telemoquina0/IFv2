13fa027b2694f466ad79e8bb648d5f10f898b4bb
telemoquina0 <38764720+telemoquina0@users.noreply.github.com>
2024-10-29 09:59:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -170,4 +170,4 @@ p.generacion()
 if __name__ == "__main__":
     #app.run(debug=True)
     #app.run(debug=False)
-    app.run()
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
