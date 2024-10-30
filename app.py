b7b8ac607b6ff6eb566d6cc103088766f08da4a9
telemoquina0 <38764720+telemoquina0@users.noreply.github.com>
2024-10-30 12:06:28 +0100

    Update app.py
    comentada y descomentada línea de app.run

--- app.py
+++ app.py
@@ -168,6 +168,6 @@ p=funcsif()
 p.generacion()
 
 if __name__ == "__main__":
-    #app.run(debug=True)
+    app.run(debug=True)
     #app.run(debug=False)
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
